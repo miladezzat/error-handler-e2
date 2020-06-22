d7b1a73ee44a1dddb32c5b2d784b929889a70894
Milad E. Fahmy <miladezzat.f@gmail.com>
2020-06-22 23:28:26 +0200

    v1.1.0

---
+++ index.js
@@ -0,0 +1,20 @@
+const error = (message, status) => {
+    let error_type;
+    if (status >= 40 && status < 500) {
+        error_type = "Client Error.";
+    } else if(status >= 500 && status < 600){
+        error_type = " Server Error.";
+    } else{
+        error_type = `${status}, it should not be a error status code`;
+    }
+
+    return {
+        message,
+        status,
+        error_type,
+    };
+}
+
+module.exports = {
+    error
+}
