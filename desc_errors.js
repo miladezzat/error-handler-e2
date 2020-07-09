904b90d01b8d418d759d0319e761bae05059772f
Milad E. Fahmy <miladezzat.f@gmail.com>
2020-07-09 21:21:29 +0200

    add feature

--- desc_errors.js
+++ desc_errors.js
@@ -262,6 +262,13 @@ const spec_errors = [
         put on the request.
         `
     },
+    {
+        statusCode: 413,
+        message:"413 Request Entity Too Large",
+        description:`The server is refusing to process a request because the request entity is larger than the server is willing or able to process. The server MAY close the connection to prevent the client from continuing the request.
+        
+        If the condition is temporary, the server SHOULD include a Retry- After header field to indicate that it is temporary and after what time the client MAY try again.`
+    },
 ]
 
 const getError = (statusCode) => {
