0328df0abff3dc8bd7c147062915dbe7c5530692
Milad E. Fahmy <miladezzat.f@gmail.com>
2021-09-02 19:46:13 +0200

    feat: update new feature

--- src/status-codes/index.ts
+++ src/status-codes/index.ts
@@ -145,6 +145,84 @@ export const ALREADY_REPORTED = 208;
 export const IM_USED = 226;
 
 /**
+ * @descriptionIndicates multiple options for the resource that the client may follow.
+ * It, for instance, could be used to present different format options for video,
+ * list files with different extensions, or word sense disambiguation.
+ *
+ * @link https://miladezzat.github.io/300.html
+ */
+export const MULTIPLE_CHOICES = 300;
+
+/**
+ * @description This and all future requests should be directed to the given URI.
+ *
+ * @link https://miladezzat.github.io/301.html
+ */
+export const MOVED_PERMANENTLY = 301;
+
+/**
+ * @description This is an example of industry practice contradicting the standard.[2] The HTTP/1.0
+ * specification (RFC 1945) required the client to perform a temporary redirect
+ * (the original describing phrase was "Moved Temporarily"), but popular browsers implemented 302
+ * with the functionality of a 303 See Other. Therefore, HTTP/1.1 added status codes 303 and 307
+ * to distinguish between the two behaviours.
+ * However, some Web applications and frameworks use the 302 status code as if it were the 303.
+ *
+ * @link https://miladezzat.github.io/302.html
+ */
+export const FOUND = 302;
+
+/**
+ * @description The response to the request can be found under another URI using a GET method.
+ * When received in response to a POST (or PUT/DELETE), it should be assumed that the server has
+ * received the data and the redirect should be issued with a separate GET message. Since HTTP/1.1
+ *
+ * @link https://miladezzat.github.io/303.html
+ */
+export const SEE_OTHER = 303;
+
+/**
+ * @description Indicates the resource has not been modified since last requested.
+ * Typically, the HTTP client provides a header like the If-Modified-Since header to provide
+ * a time against which to compare. Using this saves bandwidth and reprocessing on both the server and client,
+ * as only the header data must be sent and received in comparison to the entirety of the page being
+ * re-processed by the server, then sent again using more bandwidth of the server and client.
+ * Used for conditional GET calls to reduce band-width usage. If used, must set the Date,
+ * Content-Location, ETag headers to what they would have been on a regular GET call.
+ * There must be no body on the response.
+ *
+ * @link https://miladezzat.github.io/304.html
+ */
+export const NOT_MODIFIED = 304;
+
+/**
+ * @description Many HTTP clients (such as Mozilla and Internet Explorer) do not correctly handle responses
+ * with this status code, primarily for security reasons.
+ *
+ * @link https://miladezzat.github.io/305.html
+ */
+export const USE_PROXY = 305;
+
+/**
+ * @description In this case, the request should be repeated with another URI; however,
+ * future requests can still use the original URI. In contrast to 302, the request method should
+ * not be changed when reissuing the original request. For instance,
+ * a POST request must be repeated using another POST request.
+ *
+ * @link https://miladezzat.github.io/307.html
+ */
+export const TEMPORARY_REDIRECT = 307;
+
+/**
+ * @description The request, and all future requests should be repeated using another URI. 307 and 308
+ * (as proposed) parallel the behaviours of 302 and 301, but do not require the HTTP method to change.
+ * So, for example, submitting a form to a permanently redirected resource may continue smoothly.
+ *
+ * @link https://miladezzat.github.io/308.html
+ */
+export const PERMANENT_REDIRECT = 308;
+
+/**
  * @description The server cannot or will not process the request due to something that is perceived to be a client
  * error (e.g., malformed request syntax, invalid request message framing, or deceptive request routing).
  *
@@ -161,6 +239,17 @@ export const BAD_REQUEST = 400;
  * @link https://miladezzat.github.io/401.html
  */
 export const UNAUTHENTICATED = 401;
+
+/**
+ * @description Reserved for future use. The original intention was that this code might be used
+ * as part of some form of digital cash or micropayment scheme, but that has not happened,
+ * and this code is not usually used. As an example of its use, however,
+ * Apple's MobileMe service generates a 402 error ("httpStatusCode:402" in the Mac OS X Console log)
+ * if the MobileMe account is delinquent.
+ *
+ * @link https://miladezzat.github.io/402.html
+ */
+export const PAYMENT_REQUIRED = 402;
 
 /**
  * @description The server understood the request but refuses to authorize it.
@@ -180,6 +269,42 @@ export const UNAUTHORIZED = 403;
 export const NOT_FOUND = 404;
 
 /**
+ * @description A request was made of a resource using a request method not supported by that resource;
+ *  for example, using GET on a form which requires data to be presented via POST, or using PUT on a read-only resource.
+ *
+ * @link https://miladezzat.github.io/405.html
+ */
+
+export const METHOD_NOT_ALLOWED = 405;
+
+/**
+ * @description The requested resource is only capable of generating content not acceptable according to
+ * the Accept headers sent in the request.
+ *
+ *
+ * @link https://miladezzat.github.io/406.html
+ */
+export const NOT_ACCEPTABLE = 406;
+
+/**
+ * @description The client must first authenticate itself with the proxy.
+ *
+ *
+ * @link https://miladezzat.github.io/407.html
+ */
+export const PROXY_AUTHENTICATION_REQUIRED = 407;
+
+/**
+ * @description The server timed out waiting for the request. According to W3 HTTP specifications:
+ * "The client did not produce a request within the time that the server was prepared to wait.
+ *  The client MAY repeat the request without modifications at any later time."
+ *
+ *
+ * @link https://miladezzat.github.io/408.html
+ */
+export const REQUEST_TIMEOUT = 408;
+
+/**
  * @description The request could not be completed due to a conflict with the current state of the target resource.
  * This code is used in situations where the user might be able to resolve the conflict and resubmit the request.
  *
@@ -187,6 +312,86 @@ export const NOT_FOUND = 404;
  */
 export const CONFLICT = 409;
 
+/**
+ * @description Indicates that the resource requested is no longer available and will not be available again.
+ * This should be used when a resource has been intentionally removed and the resource should be purged.
+ * Upon receiving a 410 status code, the client should not request the resource again in the future.
+ * Clients such as search engines should remove the resource from their indices.
+ * Most use cases do not require clients and search engines to purge the resource, and a "404 Not Found"
+ * may be used instead.
+ *
+ * @link https://miladezzat.github.io/410.html
+ */
+export const GONE = 410;
+
+/**
+ * @description The request did not specify the length of its content, which is required by the requested resource.
+ *
+ * @link https://miladezzat.github.io/411.html
+ */
+export const LENGTH_REQUIRED = 411;
+
+/**
+ * @description The server does not meet one of the preconditions that the requester put on the request.
+ *
+ * @link https://miladezzat.github.io/412.html
+ */
+export const PRECONDITION_FAILED = 412;
+
+/**
+ * @description The request is larger than the server is willing or able to process.
+ *
+ * @link https://miladezzat.github.io/413.html
+ */
+export const PAYLOAD_TOO_LARGE = 413;
+
+/**
+ * @description The URI provided was too long for the server to process.
+ *
+ * @link https://miladezzat.github.io/414.html
+ */
+export const REQUEST_URI_TOO_LONG = 414;
+
+/**
+ * @description The request entity has a media type which the server or resource does not support.
+ * For example, the client uploads an image as image/svg+xml, but the server requires
+ * that images use a different format.
+ *
+ * @link https://miladezzat.github.io/415.html
+ */
+export const UNSUPPORTED_MEDIA_TYPE = 415;
+
+/**
+ * @description The client has asked for a portion of the file, but the server cannot supply that portion.
+ * For example, if the client asked for a part of the file that lies beyond the end of the file.
+ *
+ * @link https://miladezzat.github.io/416.html
+ */
+export const REQUEST_RANGE_NOT_SATISFIABLE = 416;
+
+/**
+ * @description The server cannot meet the requirements of the Expect request-header field.
+ *
+ * @link https://miladezzat.github.io/417.html
+ */
+export const EXPECTATION_FAILED = 417;
+
+/**
+ * @description This code was defined in 1998 as one of the traditional IETF April Fools' jokes,
+ * in RFC 2324, Hyper Text Coffee Pot Control Protocol, and is not expected to be implemented
+ * by actual HTTP servers. However, known implementations do exist. An Nginx HTTP server uses
+ * this code to simulate goto-like behaviour in its configuration.
+ *
+ * @link https://miladezzat.github.io/418.html
+ */
+export const I_AM_TEAPOT = 418;
+
+/**
+ * @description
+ *
+ * @link https://miladezzat.github.io/421.html
+ */
+export const MISDIRECTED_REQUEST = 421;
 /**
  * @description The server understands the content type of the request entity
  * (hence a 415 Unsupported Media Type status code is inappropriate),
@@ -198,8 +403,162 @@ export const CONFLICT = 409;
 export const UNPROCURABLE_ENTITY = 422;
 
 /**
+ * @description The resource that is being accessed is locked.
+ *
+ * @link https://miladezzat.github.io/423.html
+ */
+export const LOCKED = 423;
+
+/**
+ * @description The request failed due to failure of a previous request (e.g. a PROPPATCH).
+ *
+ * @link https://miladezzat.github.io/424.html
+ */
+export const FAILED_DEPENDENCY = 424;
+
+/**
+ * @description The client should switch to a different protocol such as TLS/1.0.
+ *
+ * @link https://miladezzat.github.io/426.html
+ */
+export const UPGRADE_REQUIRED = 426;
+
+/**
+ * @description The origin server requires the request to be conditional. Intended to prevent "the "lost update"
+ * problem, where a client GETs a resource's state, modifies it, and PUTs it back to the server, when meanwhile
+ * a third party has modified the state on the server, leading to a conflict.
+ *
+ * @link https://miladezzat.github.io/428.html
+ */
+export const PRECONDITION_REQUIRED = 428;
+
+/**
+ * @description The user has sent too many requests in a given amount of time.
+ * Intended for use with rate limiting schemes.
+ *
+ * @link https://miladezzat.github.io/429.html
+ */
+export const TOO_MANY_REQUESTS = 429;
+
+/**
+ * @description The server is unwilling to process the request because either an individual header field,
+ * or all the header fields collectively, are too large.
+ *
+ * @link https://miladezzat.github.io/431.html
+ */
+export const REQUEST_HEADER_FIELDS_TOO_LARGE = 431;
+
+/**
+ * @description An Nginx HTTP server extension. The server returns no information to the client and closes
+ * the connection (useful as a deterrent for malware).
+ *
+ * @link https://miladezzat.github.io/444.html
+ */
+export const CONNECTION_CLOSED_WITHOUT_RESPONSE = 444;
+
+/**
+ * @description Intended to be used when resource access is denied for legal reasons, e.g. censorship or
+ * government-mandated blocked access. A reference to the 1953 dystopian novel Fahrenheit 451,
+ * where books are outlawed, and the autoionization temperature of paper, 451°F.
+ *
+ * @link https://miladezzat.github.io/451.html
+ */
+export const UN_AVAILABLE_FOR_LEGAL_REASONS = 451;
+
+/**
+ * @description An Nginx HTTP server extension. This code is introduced to log the case when
+ * the connection is closed by client while HTTP server is processing its request, making server
+ * unable to send the HTTP header back.
+ *
+ * @link https://miladezzat.github.io/499.html
+ */
+export const CLIENT_CLOSED_REQUEST = 499;
+
+/**
  * @description The server encountered an unexpected condition that prevented it from fulfilling the request.
  *
  * @link https://miladezzat.github.io/500.html
  */
 export const INTERNAL_SERVER_ERROR = 500;
+
+/**
+ * @description The server either does not recognize the request method, or it lacks the ability to fulfill the request.
+ *
+ * @link https://miladezzat.github.io/501.html
+ */
+export const NOT_IMPLEMENTED = 501;
+
+/**
+ * @description The server is currently unavailable (because it is overloaded or down for maintenance).
+ * Generally, this is a temporary state.
+ *
+ * @link https://miladezzat.github.io/502.html
+ */
+export const BAD_GATEWAY = 502;
+
+/**
+ * @description
+ *
+ * @link https://miladezzat.github.io/503.html
+ */
+export const SERVICE_UNAVAILABLE = 503;
+
+/**
+ * @description The server was acting as a gateway or proxy and did not receive a timely response
+ * from the upstream server.
+ *
+ * @link https://miladezzat.github.io/504.html
+ */
+export const GATEWAY_TIMEOUT = 504;
+
+/**
+ * @description The server does not support the HTTP protocol version used in the request.
+ *
+ * @link https://miladezzat.github.io/505.html
+ */
+export const HTTP_VERSION_NOT_SUPPORTED = 505;
+
+/**
+ * @description Transparent content negotiation for the request results in a circular reference.
+ *
+ * @link https://miladezzat.github.io/506.html
+ */
+export const VARIANT_ALSO_NEGOTIATES = 506;
+
+/**
+ * @description The server is unable to store the representation needed to complete the request.
+ *
+ * @link https://miladezzat.github.io/507.html
+ */
+export const INSUFFICIENT_STORAGE = 507;
+
+/**
+ * @description The server detected an infinite loop while processing the request (sent in lieu of 208).
+ *
+ * @link https://miladezzat.github.io/508.html
+ */
+export const LOOP_DETECTED = 508;
+
+/**
+ * @description Further extensions to the request are required for the server to fulfill it.
+ *
+ * @link https://miladezzat.github.io/510.html
+ */
+export const NOT_EXTENDED = 510;
+
+/**
+ * @description The client needs to authenticate to gain network access. Intended for use by intercepting
+ * proxies used to control access to the network (e.g., "captive portals" used to require agreement
+ * to Terms of Service before granting full Internet access via a Wi-Fi hotSpot).
+ *
+ * @link https://miladezzat.github.io/511.html
+ */
+export const NETWORK_AUTHENTICATION_REQUIRED = 511;
+
+/**
+ * @description This status code is not specified in any RFCs, but is used by some HTTP
+ * proxies to signal a network connect timeout behind the proxy to a client in front of the proxy.
+ *
+ * @link https://miladezzat.github.io/599.html
+ */
+export const NETWORK_CONNECT_TIMEOUT_ERROR = 599;
